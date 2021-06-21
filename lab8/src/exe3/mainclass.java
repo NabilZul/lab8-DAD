3f81d6e3b3989993a872d74acf458dffb40434d1
NabilZul <80871331+NabilZul@users.noreply.github.com>
2021-06-21 23:51:02 +0800

    Update mainclass.java

--- lab8/src/exe3/mainclass.java
+++ lab8/src/exe3/mainclass.java
@@ -8,13 +8,13 @@ public class mainclass {
 		Runnable display3 = new Suspending();
 		
 		
-		Thread DT3 = new Thread(display1,"text");
-		Thread DT4 = new Thread(display2,"word1");
-		Thread DT5 = new Thread(display3,"word2");
+		Thread DT3 = new Thread(display1,"word1");
+		Thread DT4 = new Thread(display2,"word2");
+		Thread DT5 = new Thread(display3,"text");
 		
 		DT3.start();
-		DT4.start();
 		DT5.start();
+		DT4.start();
 		
 	}
 
